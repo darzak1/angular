0ccddc967490e6a0b72d4e3d0bde278f170ef8cc
darzak1 <darzak@mail.com>
2019-04-07 12:37:35 +0200

    zadanie 2

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { FilmyComponent } from './filmy/filmy.component';
+import {RouterModule, Routes} from "@angular/router";
+import {KsiazkiComponent} from "./ksiazki/ksiazki.component";
+
+const appRoutes: Routes = [
+  {path: 'filmy', component: FilmyComponent},
+  {path: 'ksiazki', component: KsiazkiComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilmyComponent,
+    KsiazkiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
